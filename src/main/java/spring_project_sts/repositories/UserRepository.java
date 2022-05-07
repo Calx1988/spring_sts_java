98556d63814e11b649455e92c9a391573e53e331
Calx1988 <cassioalexsander@gmail.com>
2022-05-06 22:51:54 -0300

    Projeto completo revisado

--- src/main/java/spring_project_sts/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package spring_project_sts.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import spring_project_sts.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
